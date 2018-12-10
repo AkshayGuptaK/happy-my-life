488d59022be32a21a7788ec83a6b13b97d6ad667
Akshay Gupta <akshaygrkesh@gmail.com>
2018-12-10 12:54:45 +0530

    Placeholder app component

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+// import { Route, Redirect, Switch } from 'react-router-dom'
+
+import '../App.css'
+
+class App extends React.Component {
+
+}
+
+export default hot(module)(App)
