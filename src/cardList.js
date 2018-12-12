f40703776ab720831d3eea7bb6ce3977c1c34a46
Akshay Gupta <akshaygrkesh@gmail.com>
2018-12-12 15:06:39 +0530

    Reward styling

--- src/cardList.js
+++ src/cardList.js
@@ -2,5 +2,9 @@ module.exports = {
   'banana': { 'name': 'Banana', 'type': 'Staple-Consumable', 'text': 'Gain 1 Energy', 'energy': 0, 'happiness': 1, 'texture': 'banana' },
   'waterbottle': { 'name': 'Water Bottle', 'type': 'Staple-Consumable', 'text': 'Splash!', 'energy': 0, 'happiness': 1, 'texture': 'water' },
   'raincheck': { 'name': 'Rain Check', 'type': 'Staple-Consumable', 'text': 'Draft a card and draw a card', 'energy': 0, 'happiness': 0, 'texture': 'raincheck' },
-  'sadmemory': { 'name': 'Sad Memory', 'type': 'Staple-Memory', 'text': 'Discard at end of turn', 'energy': 0, 'happiness': -1, 'texture': 'sad' }
+  'sadmemory': { 'name': 'Sad Memory', 'type': 'Staple-Memory', 'text': 'Discard at end of turn', 'energy': 0, 'happiness': -1, 'texture': 'sad' },
+  'boxofsweets': { 'name': 'Box of Sweets', 'type': 'Reward-Consumable', 'text': 'Gain 4 Energy, Trash at end of turn', 'energy': 0, 'happiness': 1, 'texture': 'reward' },
+  'blueberrypie': { 'name': 'Blueberry Pie', 'type': 'Reward-Consumable', 'text': 'Gain 2 Energy', 'energy': 0, 'happiness': 2, 'texture': 'reward' },
+  'goodtime': { 'name': 'A Good Time', 'type': 'Reward-Memory', 'text': 'Discard at end of turn', 'energy': 0, 'happiness': 4, 'texture': 'reward' },
+  'happymemory': { 'name': 'Happy Memory', 'type': 'Reward-Memory', 'text': 'Discard at end of turn', 'energy': 0, 'happiness': 6, 'texture': 'reward' }
 }
