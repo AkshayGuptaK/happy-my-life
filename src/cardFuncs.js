a7c6679ef95582b48bec054de9f6c487e3a8904e
Akshay Gupta <akshaygrkesh@gmail.com>
2018-12-14 14:34:15 +0530

    Fixed reward buying

--- src/cardFuncs.js
+++ src/cardFuncs.js
@@ -1,14 +1,14 @@
 module.exports = {
   'banana': changePlayerStateFuncFactory(1, 'energy'),
   'waterbottle': noChange,
-  'raincheck': changePlayerStateFuncFactory(1, 'drafts'),
+  'raincheck': changePlayerStateFuncFactory(1, 'draft'),
   'boxofsweets': changePlayerStateFuncFactory(4, 'energy'),
   'blueberrypie': changePlayerStateFuncFactory(2, 'energy'),
   'brainstorm': noChange,
   'hardwork': changePlayerStateFuncFactory(2, 'effort'),
   'runerrand': changePlayerStateFuncFactory(1, 'effort'),
-  'study': changePlayerStateFuncFactory(1, 'drafts'),
-  'train': changePlayerStateFuncFactory(2, 'drafts')
+  'study': changePlayerStateFuncFactory(1, 'draft'),
+  'train': changePlayerStateFuncFactory(2, 'draft')
 }
 
 function noChange (state) { // placeholder function for not implemented cards
