e77fb845e3525a120f362a245aa9d0a2d0523fb3
Akshay Gupta <akshaygrkesh@gmail.com>
2018-12-21 12:49:26 +0530

    Fixed Rain Check

--- src/gameFunctions.js
+++ src/gameFunctions.js
@@ -1,7 +1,7 @@
 exports.cardFuncs = {
   'banana': changePlayerStateFuncFactory(1, 'energy'),
   'waterbottle': noChange,
-  'raincheck': changePlayerStateFuncFactory(1, 'draft'), // add draw
+  'raincheck': composeStateFunc(changePlayerStateFuncFactory(1, 'draft'), draw), // add draw
   'boxofsweets': changePlayerStateFuncFactory(4, 'energy'),
   'blueberrypie': changePlayerStateFuncFactory(2, 'energy'),
   'brainstorm': noChange,
@@ -11,19 +11,26 @@ exports.cardFuncs = {
   'train': changePlayerStateFuncFactory(2, 'draft')
 }
 
-exports.draw = function draw (state) {
+exports.draw = draw
+exports.shuffle = shuffle
+
+function composeStateFunc (func1, func2) {
+  return (x) => func1(func2(x))
+}
+
+function draw (state) {
   if (state.deck.length > 0) {
     state.hand.push(state.deck.shift())
     return state
   }
   if (state.discard.length > 0) {
-    state.deck = this.shuffle(state.discard)
+    state.deck = shuffle(state.discard)
     state.discard = []
     return draw(state)
   } return state
 }
 
-exports.shuffle = function shuffle (arr) {
+function shuffle (arr) {
   for (let i = arr.length - 1; i > 0; i--) {
     let index = Math.floor(Math.random() * (i + 1))
     let current = arr[i]
